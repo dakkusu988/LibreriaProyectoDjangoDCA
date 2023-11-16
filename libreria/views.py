cae42080861356143d9b4752c345d5cf0aa1020c
dakkusu988 <bydaxplayyt@gmail.com>
2023-11-16 12:25:30 +0100

    creacion (listado, detalles, editar, añadir).html

--- libreria/views.py
+++ libreria/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from .forms import LibreriaForm
+from .models import Libreria
+"""
+class Formulario(View):
+    Libreria_template = 'Libreria/listado.html'
 
-# Create your views here.
+    def get(self,request):
+        form = LibreriaForm()
+        return render(request, self.Libreria_template, {'form': form})
+    
+    def post(self,request):
+        form = LibreriaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('confirmacion')
+        return render(request, self.Libreria_template, {'form': form})
+
+class Confirmacion(View):
+    confirmacion_template = 'Libreria/confirmacion.html'
+
+    def get(self,request):
+        return render(request, self.confirmacion_template)
+
+class Lista_Libreria(View):
+    lista_Libreria_template = 'Libreria/lista_Libreria.html'
+
+    def get(self,request):
+        Libreria = Libreria.objects.all()
+        return render(request, self.lista_Libreria_template, {'Libreria': Libreria})
+"""
