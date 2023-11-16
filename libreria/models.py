4705f84ca56cee3607d09c77115b8ca96ac2cb16
dakkusu988 <bydaxplayyt@gmail.com>
2023-11-16 11:15:17 +0100

    creacion y modificacion de models y migracion done

--- libreria/models.py
+++ libreria/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Libreria(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    rating = models.IntegerField
+    created_at = models.DateField
+    updated_at = models.DateField
+
+    def __str__(self):
+        return self.title
