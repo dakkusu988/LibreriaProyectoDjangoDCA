de02f4ff1e1871ab5bea5c7fea637b05e054afd4
dakkusu988 <bydaxplayyt@gmail.com>
2023-11-16 12:57:40 +0100

    por defecto las fechas en models

--- libreria/models.py
+++ libreria/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from datetime import date
 
 class Libreria(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
     rating = models.IntegerField(validators = [MinValueValidator(0), MaxValueValidator(5)])
-    created_at = models.DateField()
-    updated_at = models.DateField()
+    created_at = models.DateField(default=date.today)
+    updated_at = models.DateField(default=date.today)
 
     def __str__(self):
         return self.title
