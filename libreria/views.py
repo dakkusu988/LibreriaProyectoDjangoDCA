4c1f857ae3f359694237fa4724ab54cadb3f50e2
dakkusu988 <bydaxplayyt@gmail.com>
2023-11-16 12:44:44 +0100

    cambio en los html y prueba en view

--- libreria/views.py
+++ libreria/views.py
@@ -2,31 +2,37 @@ from django.shortcuts import render, redirect
 from django.views import View
 from .forms import LibreriaForm
 from .models import Libreria
-"""
-class Formulario(View):
-    Libreria_template = 'Libreria/listado.html'
+
+class Listado(View):
+    libreria_template = 'libreria/listado.html'
 
     def get(self,request):
         form = LibreriaForm()
-        return render(request, self.Libreria_template, {'form': form})
+        return render(request, self.libreria_template, {'form': form})
     
     def post(self,request):
         form = LibreriaForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('confirmacion')
-        return render(request, self.Libreria_template, {'form': form})
+            return redirect('detalles')
+        return render(request, self.libreria_template, {'form': form})
 
-class Confirmacion(View):
-    confirmacion_template = 'Libreria/confirmacion.html'
+class Detalles(View):
+    confirmacion_template = 'libreria/detalles.html'
 
     def get(self,request):
         return render(request, self.confirmacion_template)
 
-class Lista_Libreria(View):
-    lista_Libreria_template = 'Libreria/lista_Libreria.html'
+class Editar(View):
+    lista_libreria_template = 'libreria/editar.html'
 
     def get(self,request):
-        Libreria = Libreria.objects.all()
-        return render(request, self.lista_Libreria_template, {'Libreria': Libreria})
-"""
+        libreria = libreria.objects.all()
+        return render(request, self.lista_libreria_template, {'libreria': libreria})
+
+class Añadir(View):
+    lista_libreria_template = 'libreria/añadir.html'
+
+    def get(self,request):
+        libreria = libreria.objects.all()
+        return render(request, self.lista_libreria_template, {'libreria': libreria})
