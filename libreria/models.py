ab5b9db8e4634ee1238bb3f1eac61925e552c71d
dakkusu988 <bydaxplayyt@gmail.com>
2023-11-16 13:00:26 +0100

    cambios en por defectos fechas

--- libreria/models.py
+++ libreria/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
-from datetime import date
 
 class Libreria(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
     rating = models.IntegerField(validators = [MinValueValidator(0), MaxValueValidator(5)])
-    created_at = models.DateField(default=date.today)
-    updated_at = models.DateField(default=date.today)
+    created_at = models.DateField()
+    updated_at = models.DateField()
 
     def __str__(self):
         return self.title
