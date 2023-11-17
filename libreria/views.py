129818a83b8e545277c1421cefca30d53a736461
dakkusu988 <bydaxplayyt@gmail.com>
2023-11-17 11:27:50 +0100

    pruebas en vista y css en HTMLs

--- libreria/views.py
+++ libreria/views.py
@@ -2,41 +2,40 @@ from django.shortcuts import render, redirect
 from django.views import View
 from .forms import LibreriaForm
 from .models import Libreria
+from django.views.generic import ListView, DetailView
 
 class Listado(View):
     libreria_template = 'libreria/listado.html'
-"""
+
     def get(self,request):
         form = LibreriaForm()
         return render(request, self.libreria_template, {'form': form})
     
+class Detalles(View):
+    libreria_template = 'libreria/detalles.html'
+
+    def get(self,request):
+        return render(request, self.libreria_template)
+
+
+class Editar(View):
+    libreria_template = 'libreria/editar.html'
+
+    def get(self,request):
+        libreria = Libreria.objects.all()
+        return render(request, self.libreria_template, {'libreria': libreria})
+
+
+class Añadir(View):
+    libreria_template = 'libreria/añadir.html'
+
+    def get(self,request):
+        libreria = Libreria.objects.all()
+        return render(request, self.libreria_template, {'libreria': libreria})
+
     def post(self,request):
         form = LibreriaForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('detalles')
         return render(request, self.libreria_template, {'form': form})
-"""
-
-class Detalles(View):
-    libreria_template = 'libreria/detalles.html'
-"""
-    def get(self,request):
-        return render(request, self.confirmacion_template)
-"""
-
-class Editar(View):
-    libreria_template = 'libreria/editar.html'
-"""
-    def get(self,request):
-        libreria = libreria.objects.all()
-        return render(request, self.lista_libreria_template, {'libreria': libreria})
-"""
-
-class Añadir(View):
-    libreria_template = 'libreria/añadir.html'
-"""
-    def get(self,request):
-        libreria = libreria.objects.all()
-        return render(request, self.lista_libreria_template, {'libreria': libreria})
-"""
